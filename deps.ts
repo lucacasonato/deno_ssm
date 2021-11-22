10674eea14377046784f7f9fdf81740125e917c6
Luca Casonato <hello@lcas.dev>
2021-11-23 00:20:31 +0100

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,1 @@
-export { AWSSignerV4 } from "https://deno.land/x/aws_sign_v4@0.1.1/mod.ts";
-import { createHash } from "https://deno.land/std@0.84.0/hash/mod.ts";
-export function sha256Hex(data: string | Uint8Array): string {
-  const hasher = createHash("sha256");
-  hasher.update(data);
-  return hasher.toString("hex");
-}
+export { AWSSignerV4 } from "https://deno.land/x/aws_sign_v4@1.0.2/mod.ts";
